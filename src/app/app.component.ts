91a78f8d44579cf693e0237d122463a97e1349ad
Borja Arana <barana1977@hotmail.com>
2021-04-08 12:22:36 +0200

    Setup init app. Added playlist module. Added store to state management playlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Store } from './playlist/store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'store-observable';
+  todos$ = this.store.select<any[]>('playlist');
+
+  constructor( private store: Store ) {
+    this.store.set('playlist', [
+      { id: 1, name: 'All The Small Things' },
+      { id: 2, name: 'Wish You Were Here' },
+    ]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Store } from './playlist/store';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [Store],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
